f09485574bc37dcaa84d4e8f5964f6d7a964734e
brian@tannerpages.com <brian@tannerpages.com@534b8df9-34de-497c-b096-3b0b056f24ee>
2007-12-17 19:31:35 +0000

    updated proving jar
    git-svn-id: file:///Users/btanner/Downloads/rescued-repos/svn/rl-competition/trunk@111 534b8df9-34de-497c-b096-3b0b056f24ee

--- proving/debug.bash
+++ proving/debug.bash
@@ -12,5 +12,5 @@ compLib=$libPath/RLVizLib.jar
 echo "Starting up proving software..."
 export LC_ALL=en_US
 macAboutNameCommand=-Dcom.apple.mrj.application.apple.menu.about.name=RLCompetitionProvingApp
-java -Xmx128M  -DofflineStub=true $macAboutNameCommand -jar ./bin/Proving.jar
+java -Xmx128M  -DofflineStub=true -agentlib:Shark $macAboutNameCommand -jar ./bin/Proving.jar
 echo "-- Proving software finished"
