c039a4b2448527435a112d33fa1805e243c01328
brian@tannerpages.com <brian@tannerpages.com@534b8df9-34de-497c-b096-3b0b056f24ee>
2008-02-29 23:08:40 +0000

    fixed the run script to work with cygwin I hope
    git-svn-id: file:///Users/btanner/Downloads/rescued-repos/svn/rl-competition/trunk@204 534b8df9-34de-497c-b096-3b0b056f24ee

--- trainers/consoleTrainerJava/run.bash
+++ trainers/consoleTrainerJava/run.bash
@@ -3,13 +3,13 @@ basePath=../..
 systemPath=$basePath/system
 #Source a script that sets all important functions and variables
 source $systemPath/rl-competition-includes.sh
-
+localMainPath=$VIZ_CLASSPATH:./bin/
 
 #Utility functions from rl-competition-includes.sh
 startRLGlueInBackGround
 startEnvShellInBackGround
 
-java -Xmx128M -classpath $VIZ_CLASSPATH:./bin/ consoleTrainer
+java -Xmx128M -classpath $localMainPath consoleTrainer
 
 #Utility functions from rl-competition-includes.sh
 waitForEnvShellToDie
