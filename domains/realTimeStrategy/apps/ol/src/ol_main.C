4adbbf9efd096a552a453b77383e706089dd3aac
marc.lanctot <marc.lanctot@534b8df9-34de-497c-b096-3b0b056f24ee>
2008-04-14 19:06:06 +0000

    bug in loading the parms file
    git-svn-id: file:///Users/btanner/Downloads/rescued-repos/svn/rl-competition/trunk@227 534b8df9-34de-497c-b096-3b0b056f24ee

--- domains/realTimeStrategy/apps/ol/src/ol_main.C
+++ domains/realTimeStrategy/apps/ol/src/ol_main.C
@@ -89,7 +89,7 @@ int main(int argc, char ** argv)
   
   MiniGameState s;
   MiniGameParameters gp;
-  if (parmsfile != NULL);
+  if (parmsfile != NULL)
     load_parms(parmsfile, &gp);
   
   if (trace != NULL)
